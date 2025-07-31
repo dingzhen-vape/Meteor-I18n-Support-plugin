1e84cfe03f77cd964f5e061c16cac5e14d340c4f
dingzhen-vape <327112257@qq.com>
2025-07-31 11:30:22 +0800

    refactor(ui): 更新包名并添加翻译功能

--- src/main/java/com/yalu/addon/modules/AboutThisPlugin.java
+++ src/main/java/com/yalu/addon/modules/AboutThisPlugin.java
@@ -1,26 +1,10 @@
 package com.yalu.addon.modules;
 
 import com.yalu.addon.TranslateAddon;
-import meteordevelopment.meteorclient.events.render.Render3DEvent;
-import meteordevelopment.meteorclient.renderer.ShapeMode;
-import meteordevelopment.meteorclient.settings.ColorSetting;
-import meteordevelopment.meteorclient.settings.DoubleSetting;
-import meteordevelopment.meteorclient.settings.Setting;
-import meteordevelopment.meteorclient.settings.SettingGroup;
 import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.meteorclient.utils.player.ChatUtils;
-import meteordevelopment.meteorclient.utils.render.color.Color;
-import meteordevelopment.meteorclient.utils.render.color.SettingColor;
-import meteordevelopment.orbit.EventHandler;
-import net.minecraft.datafixer.fix.TextComponentHoverAndClickEventFix;
-import net.minecraft.datafixer.fix.TextComponentStringyFlagsFix;
 import net.minecraft.text.ClickEvent;
 import net.minecraft.text.Text;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Box;
-
-import javax.swing.text.Style;
-import javax.swing.text.StyleContext;
 import java.net.URI;
 
 public class AboutThisPlugin extends Module {
@@ -37,7 +21,8 @@ public class AboutThisPlugin extends Module {
             "\n可惜的时汉化版暂不支持自定义字体" +
             "\n使用AI机翻+人工校验，若有出错请到github仓库上提交issue" +
             "\n本插件完全免费且开源，请勿用于商业用途，且不要上当受骗" +
-            "\n如果发现插件汉化不全或出现未汉化的问题请查看运行目录下的lang.json或lang/zh_cn.json文件自行汉化"));
+            "\n如果发现插件汉化不全或出现未汉化的问题请查看版本文件夹根目录下的lang.json或lang/zh_cn.json文件自行汉化" +
+            "\n注意！如果有发现功能名称变成key的情况，请将版本文件夹根目录下的lang.json内的内容放入[插件名称].jar内assets/lang文件夹内的en_us.json内，并重启客户端即可解决"));
         ChatUtils.sendMsg(Text.literal("点我跳转译者B站主页").styled(s -> s.withClickEvent(new ClickEvent.OpenUrl(URI.create("https://space.bilibili.com/432060575?spm_id_from=333.1007.0.0")))));
         ChatUtils.sendMsg(Text.literal("点我跳转插件仓库").styled(s -> s.withClickEvent(new ClickEvent.OpenUrl(URI.create("https://github.com/dingzhen-vape/Meteor-Translate-plugin/settings")))));
     }
