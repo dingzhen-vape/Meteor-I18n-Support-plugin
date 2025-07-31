1b79594c52036c9266981076e82955a1b6fc825a
dingzhen-vape <327112257@qq.com>
2025-07-31 11:37:07 +0800

    refactor(ui): 更新包名并添加翻译功能

--- src/main/java/com/yalu/addon/modules/AboutThisPlugin.java
+++ src/main/java/com/yalu/addon/modules/AboutThisPlugin.java
@@ -5,8 +5,6 @@ import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.meteorclient.utils.player.ChatUtils;
 import net.minecraft.text.ClickEvent;
 import net.minecraft.text.Text;
-import java.net.URI;
-
 public class AboutThisPlugin extends Module {
 
     public AboutThisPlugin() {
@@ -23,7 +21,9 @@ public class AboutThisPlugin extends Module {
             "\n本插件完全免费且开源，请勿用于商业用途，且不要上当受骗" +
             "\n如果发现插件汉化不全或出现未汉化的问题请查看版本文件夹根目录下的lang.json或lang/zh_cn.json文件自行汉化" +
             "\n注意！如果有发现功能名称变成key的情况，请将版本文件夹根目录下的lang.json内的内容放入[插件名称].jar内assets/lang文件夹内的en_us.json内，并重启客户端即可解决"));
-        ChatUtils.sendMsg(Text.literal("点我跳转译者B站主页").styled(s -> s.withClickEvent(new ClickEvent.OpenUrl(URI.create("https://space.bilibili.com/432060575?spm_id_from=333.1007.0.0")))));
-        ChatUtils.sendMsg(Text.literal("点我跳转插件仓库").styled(s -> s.withClickEvent(new ClickEvent.OpenUrl(URI.create("https://github.com/dingzhen-vape/Meteor-Translate-plugin/settings")))));
+//        ChatUtils.sendMsg(Text.literal("点我跳转译者B站主页").styled(s -> s.withClickEvent(new ClickEvent.OpenUrl(URI.create("https://space.bilibili.com/432060575?spm_id_from=333.1007.0.0")))));
+//        ChatUtils.sendMsg(Text.literal("点我跳转插件仓库").styled(s -> s.withClickEvent(new ClickEvent.OpenUrl(URI.create("https://github.com/dingzhen-vape/Meteor-Translate-plugin/settings")))));
+        ChatUtils.sendMsg(Text.literal("点我跳转译者B站主页").styled(s -> s.withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL,("https://space.bilibili.com/432060575?spm_id_from=333.1007.0.0")))));
+        ChatUtils.sendMsg(Text.literal("点我跳转插件仓库").styled(s -> s.withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL,("https://github.com/dingzhen-vape/Meteor-Translate-plugin/settings")))));
     }
 }
